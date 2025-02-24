3bd5ca872d622fad29b8404a176415d619593bd0
Sabrina Belloni <sabrina82belloni@gmail.com>
2025-02-24 14:19:11 +0100

    add models and registered in admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
-from .models import Product,Category
+from .models import Product, Category
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+
+class productAdmin(admin.ModelAdmin):
+    # show the fields available in the admin pane
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+    ordering = ('sku',)
+
+
+class categoryAdmin(admin.ModelAdmin):
+    # show the fields available in the admin pane
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, productAdmin)
+admin.site.register(Category, categoryAdmin)
