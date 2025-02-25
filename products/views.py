0d1b65cbad4c665105d7b3ebd8e9188d201114a5
Sabrina Belloni <sabrina82belloni@gmail.com>
2025-02-25 13:56:33 +0100

    add search bar functionality

--- products/views.py
+++ products/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q  # generate a search query since filter method
+# requires the searched part in both description and name. q can be used for search only in the name OR description
 from .models import Product
 
 # Create your views here.
@@ -7,8 +10,21 @@ from .models import Product
 def all_products(request):
     """a view to shows all products page"""
     products = Product.objects.all()
+    query = None
+    # below the code is used for filtering the product based on a search query
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "you didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
     context = {
         'products': products,
+        'search_term': query,
     }
     return render(request, 'products/products.html', context)
 
