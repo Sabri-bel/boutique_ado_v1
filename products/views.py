252c0ba7c455c3bb5afe66d6afbf5d39144bf2da
Sabrina Belloni <sabrina82belloni@gmail.com>
2025-02-27 12:57:53 +0100

    add jquery to search field

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q  # generate a search query since filter method
 # requires the searched part in both description and name. q can be used for search only in the name OR description
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 # Create your views here.
@@ -21,9 +22,10 @@ def all_products(request):
             sortkey = request.GET['sort']
             sort = sortkey
             if sortkey == 'name':
-                sortkey = 'lower.name'
+                sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey == 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
