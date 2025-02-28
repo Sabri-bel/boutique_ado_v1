c3536bf0885b8af37079f4a1ba2c315897659250
Sabrina Belloni <sabrina82belloni@gmail.com>
2025-02-28 12:33:59 +0100

    add size on context and view

--- bag/contexts.py
+++ bag/contexts.py
@@ -12,17 +12,37 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    # below how to render the page:get the session if already or initialize a new one
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-             })
+    # iterate through all the items in the shopping bag:
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            # if item_data has no size (so it is an integer and not a dictionary):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            # below append information in the empty list created above
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+                })
+        else:
+            # if the item_data has quantity+size it is a dictionary:
+            product = get_object_or_404(product, pk=item_id)
+            
+            # we need to iterate through the inner dictionary items_by_size
+            for size, quantity in item_data['items_by_size'].items():
+                # increment the product count and total accordingly
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+                'size': size
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
--- bag/views.py
+++ bag/views.py
@@ -19,16 +19,33 @@ def add_to_bag(request, item_id):
     # 3. store the information in a session (customer can continue to browse the site and it 
     # will save the information related to the shopping bag in the actual session)
     # this session persists until user close the browser
-    #the variable bag will contain this information
+    # the variable bag will contain this information
+    size = None
+
+    # is the product has size, it will be set with this code:
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        # if bag variable already exist in the session it will be updated
-        bag[item_id] += quantity
-    else: 
-        # if the bag variable is not present in the session it will be created
-        bag[item_id] =  quantity
- 
+    if size:
+        if item_id in list(bag.keys()):
+            # if an item is already in the bag, it will update the quantity if the size is the same:
+            if size in bag[item_id]['items_by_size'].key:
+                bag[item_id]['item_by_size'][size] += quantity
+            # if the item already exist but with a different size, it will be added as is:
+            else:
+                bag[item_id]['item_by_size'][size] = quantity
+        else:
+            # if items not in the bag, it will add it using the ID and the size as dictionary
+            bag[item_id] = {'items_by_size':{size: quantity}}
+    else:
+        if item_id in list(bag.keys()):
+            # if bag variable already exist in the session it will be updated
+            bag[item_id] += quantity
+        else: 
+            # if the bag variable is not present in the session it will be created
+            bag[item_id] = quantity
+
     # 4. overwrite the information in the session (python dictionary)
     request.session['bag'] = bag
     # 5. redirect the user back to the redirect url 
