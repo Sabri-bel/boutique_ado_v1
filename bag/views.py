267dd49bab67c2ec0d637c811646497d8a63fa9b
Sabrina Belloni <sabrina82belloni@gmail.com>
2025-03-03 16:08:52 +0100

    add the update and remove from cart option

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -50,3 +50,69 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     # 5. redirect the user back to the redirect url 
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """
+    adjust the quantity of the specified item in the shopping bag
+    """
+    # 1. get a quantity from the form and convert as integer
+    quantity = int(request.POST.get('quantity'))
+    # 2. store the information in a session (customer can continue to browse the site and it 
+    # will save the information related to the shopping bag in the actual session)
+    # this session persists until user close the browser
+    # the variable bag will contain this information
+    size = None
+
+    # is the product has size, it will be set with this code:
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {})
+
+    if size:
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
+        else:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+
+    else:
+        if quantity > 0:
+            # if bag variable already exist in the session it will be updated
+            bag[item_id] = quantity
+        else: 
+            # if the bag variable is not present in the session it will be created
+            bag.pop(item_id)
+
+    # 4. overwrite the information in the session (python dictionary)
+    request.session['bag'] = bag
+    # 5. redirect the user back to the redirect url
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """
+    remove the specified item in the shopping bag
+    """
+    try:
+        size = None
+
+        # is the product has size, it will be set with this code:
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+
+        else:
+            bag.pop(item_id)
+
+        # 4. overwrite the information in the session (python dictionary)
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
