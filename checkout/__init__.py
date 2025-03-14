5707703611b95f941feb7a517577cf2471006e66
Sabrina Belloni <sabrina82belloni@gmail.com>
2025-03-14 15:28:31 +0100

    added a checkout confirmation page

---
+++ checkout/__init__.py
@@ -0,0 +1,4 @@
+# tell django the name of the defaullt config class for the app
+# this is required for let signals work in django
+# this class is present in checkout/apps.py:
+default_app_config = 'checkout.apps.CheckoutConfig'
