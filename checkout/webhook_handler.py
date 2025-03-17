e687723554c08e3ce8f33474d0e96e9aa7c9b551
Sabrina Belloni <sabrina82belloni@gmail.com>
2025-03-17 14:30:51 +0100

    add the event handler for webhook

---
+++ checkout/webhook_handler.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+
+class StripeWH_handler:
+    """
+    handle stripe webhook
+    this is a notification for each operation of stripe (such as payment
+    intent being created, payment complete and so on)
+    webhooks are like signals django send every time a model is saved or
+    deleted, but they are sent using a secure URL provided 
+    """
+    def __init__(self, request):
+        # init is called everytime an instance is created
+        # we are going to use it to assign the request as an attribute
+        # of the class
+        self.request = request
+
+    def handle_event(self, event):
+        """
+        handle a generic/unknown/unexpected webhook event
+        it will take the event of stripe and return an http response
+        """
+        return HttpResponse(
+            content=f"Webhook received: {event['type']}",
+            status=200
+        )
